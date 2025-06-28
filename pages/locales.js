9f1cbb28447a82fd68ef1c6f0346e90652e35d57
AlexeyFedotenko <alexfedotenko@yandex.ru>
2025-06-28 22:03:38 +0300

    Create locales.js

---
+++ pages/locales.js
@@ -0,0 +1,18 @@
+const locales = {
+  ru: {
+    title: "Jarvis Boost 🚀",
+    catalog: "Каталог услуг",
+    openCatalog: "Открыть каталог услуг"
+  },
+  en: {
+    title: "Jarvis Boost 🚀",
+    catalog: "Service Catalog",
+    openCatalog: "Open Service Catalog"
+  },
+  zh: {
+    title: "Jarvis Boost 🚀",
+    catalog: "服務目錄",
+    openCatalog: "打開服務目錄"
+  }
+};
+export default locales;
